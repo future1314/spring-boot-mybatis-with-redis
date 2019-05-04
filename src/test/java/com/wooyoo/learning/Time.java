9d76840fee948497b5a48b8c58282c0aebd208a7
yingchun.lou <yingchun.lou@wm-motor.com>
2019-05-04 11:58:30 +0800

    time

---
+++ src/test/java/com/wooyoo/learning/Time.java
@@ -0,0 +1,20 @@
+package com.wooyoo.learning;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class Time {
+    public static void main(String[] args) {
+        System.out.println(System.currentTimeMillis());
+        System.out.println(getTimesmorning());
+
+    }
+    public static Long getTimesmorning() {
+        Calendar cal = Calendar.getInstance();
+        cal.set(Calendar.HOUR_OF_DAY, 0);
+        cal.set(Calendar.SECOND, 0);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.MILLISECOND, 0);
+        return cal.getTime().getTime();
+    }
+}
